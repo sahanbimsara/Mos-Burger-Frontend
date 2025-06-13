1d5ef63b9aee7b435b75d7ea269e788b22ebde59
sahanbimsara <sahanbimsara2060@gmail.com>
2025-06-13 17:23:12 +0530

    first commit

---
+++ JS/app.js
@@ -0,0 +1,18 @@
+
+document.getElementById('loginForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    //demo login username and password
+    const validUsername = 'admin';
+    const validPassword = '1234';
+
+    if (username === validUsername && password === validPassword) {
+        alert('Login successful!');
+        window.location.href = 'index.html'; // Redirect to home page
+    } else {
+        alert('Invalid username or password');
+    }
+});
